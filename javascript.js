68f92491f85c5ae209f3d9b31f39592f833dc23c
Mujanov3737 <75598761+Mujanov3737@users.noreply.github.com>
2023-07-03 13:25:27 -0400

    Create function to capture user input

--- javascript.js
+++ javascript.js
@@ -5,4 +5,30 @@ function getComputerChoice() {
     return VALID_ACTIONS[(Math.floor(Math.random() * VALID_ACTIONS.length))]; 
 }
 
+function getPlayerChoice() {
+
+    let chosen = false;
+    let playerChoice;
+
+    while(chosen != true) {
+
+        playerChoice = prompt("Please enter an action - rock, paper, or scissors:");
+
+        if (playerChoice === null) {
+            alert('Thank you for playing.');
+            return;
+        }
+        else if (VALID_ACTIONS.includes(playerChoice.toLowerCase())) {
+            chosen = true;
+            return playerChoice.toLowerCase();
+        }
+        else {
+            alert('Not a valid action, please try again.');
+            continue;
+        }
+    }
+}
+
 console.log(getComputerChoice());
+let choice = getPlayerChoice();
+console.log(choice);
