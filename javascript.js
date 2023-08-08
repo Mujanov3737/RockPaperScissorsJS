de8d631299beb160acf7d43a5d82ecf354451383
Mujanov3737 <75598761+Mujanov3737@users.noreply.github.com>
2023-08-08 02:59:16 -0400

    Refactor event listener logic to remove redundancy

--- javascript.js
+++ javascript.js
@@ -10,45 +10,22 @@ function getComputerChoice() {
     return VALID_ACTIONS[(Math.floor(Math.random() * VALID_ACTIONS.length))]; 
 }
 
-let scoreAI = 0;
-let scorePlayer = 0;
+let aiScore  = 0;
+let playerScore = 0;
 let round = 1;
-let gameover = false;
 let result;
 
-const btnRock = document.querySelector('.btn-rock');
-const btnPaper = document.querySelector('.btn-paper');
-const btnScissors = document.querySelector('.btn-scissors');
-const choiceCpu = document.querySelector('.choice-cpu');
-const scoreCpuNode = document.querySelector('.score-cpu');
-const scorePlayerNode = document.querySelector('.score-player');
-const roundResult = document.querySelector('.result');
+const aiChoiceElement = document.querySelector('.choice-cpu');
+const aiScoreElement  = document.querySelector('.score-cpu');
+const playerScoreElement = document.querySelector('.score-player');
+const roundResultElement  = document.querySelector('.result');
 
 // Wrapping event listeners so that the DOM is first parsed before this part of the script executes.
-document.addEventListener('DOMContentLoaded', function() {
-    
-    btnRock.addEventListener('click', () => {
-        result = playRound(getComputerChoice(), 'rock');
-        checkRoundWin(result);
-        roundResult.textContent = result.toUpperCase();
-        scorePlayerNode.textContent = scorePlayer;
-        scoreCpuNode.textContent = scoreAI;
-    });
-
-    btnPaper.addEventListener('click', () => {
-        result = playRound(getComputerChoice(), 'paper');
-        checkRoundWin(result);
-        roundResult.textContent = result.toUpperCase();
-        scorePlayerNode.textContent = scorePlayer;
-        scoreCpuNode.textContent = scoreAI;
-    });
-
-    btnScissors.addEventListener('click', () => {
-        result = playRound(getComputerChoice(), 'scissors');
-        checkRoundWin(result);
-        roundResult.textContent = result.toUpperCase();
-        scorePlayerNode.textContent = scorePlayer;
-        scoreCpuNode.textContent = scoreAI;
+document.addEventListener('DOMContentLoaded', () => {
+    ['rock', 'paper', 'scissors'].forEach(action => {
+        document.querySelector(`.btn-${action}`).addEventListener('click', () => {
+            playAndDisplayRound(action);
+        });
     });
 });
 
@@ -58,7 +35,7 @@ function playRound (computerChoice, playerChoice) {
     console.log(`Computer's choice is: ${computerChoice}`);
     console.log(`Player's choice is: ${playerChoice}`);
 
-    choiceCpu.textContent = computerChoice.toUpperCase();
+    aiChoiceElement.textContent = computerChoice.toUpperCase();
 
     if (playerChoice === computerChoice) {
         return 'draw';
@@ -67,101 +44,30 @@ function playRound (computerChoice, playerChoice) {
         return 'win';
     }
     return 'loss';
+}
 
+// Handles logic for updating the view and checking the win result
+function playAndDisplayRound(playerChoice) {
+    const computerChoice = getComputerChoice();
+    const result = playRound(computerChoice, playerChoice);
+
+    checkRoundWin(result);
+    roundResultElement.textContent = result.toUpperCase();
+    playerScoreElement.textContent = playerScore;
+    aiScoreElement.textContent = aiScore;
 }
 
 function checkRoundWin(result) {
-    // Updates variables based on round result
+    // Updates variables based on round result. TODO add rounds
     if (result === 'win') {
-        scorePlayer += 1;
+        playerScore += 1;
         round += 1;
     }
     else if (result === 'loss') {
-        scoreAI += 1;
+        aiScore += 1;
         round += 1;
     }
     else if (result === 'draw') {
         round += 1;
     }
 }
-
-// Prompts player for a choice, validates, and returns as lowercase string
-// function getPlayerChoice() {
-
-//     let chosen = false;
-//     let playerChoice;
-
-//     while(chosen != true) {
-
-//         playerChoice = prompt("Please enter an action - rock, paper, or scissors:");
-
-//         if (playerChoice === null) {
-//             alert('Thank you for playing.');
-//             return;
-//         }
-//         else if (VALID_ACTIONS.includes(playerChoice.toLowerCase())) {
-//             chosen = true;
-//             return playerChoice.toLowerCase();
-//         }
-//         else {
-//             alert('Not a valid action, please try again.');
-//             continue;
-//         }
-//     }
-// }
-
-// Compares score to win limit
-// TODO: Add functionality for player to choose win limit, default to 5 currently
-// function checkWin(scoreAI, scorePlayer) {
-//     if (scoreAI === 5 ) {
-//         alert('You Lose!')
-//         return true;
-//     }
-//     else if (scorePlayer === 5 ) {
-//         alert('You Win!')
-//         return true;
-//     }
-//     else {
-//         return false;
-//     }
-// }
-
-// Game function
-// function game() {
-    
-//     // Main game loop
-//     while (!gameover) {
-
-//         // Checks win conditions
-//         gameover = checkWin(scoreAI, scorePlayer);
-        
-//         console.log(`Round: ${round}`);
-//         console.log(`---------------`);
-//         console.log(`Player's Score: ${scorePlayer}`);
-//         console.log(`Computer's Score: ${scoreAI}`);
-//         console.log(`---------------`);
-        
-//         // Plays a round
-//         let result = playRound(getComputerChoice(), getPlayerChoice());
-//         console.log(`Result was a: ${result}`);
-//         console.log('\n');
-
-//         // Updates variables based on round result
-//         if (result === 'win') {
-//             scorePlayer += 1;
-//             round += 1;
-//             continue;
-//         }
-//         else if (result === 'loss') {
-//             scoreAI += 1;
-//             round += 1;
-//             continue;
-//         }
-//         else if (result === 'draw') {
-//             round += 1;
-//             continue;
-//         }
-//     }
-// }
-
-//game();
